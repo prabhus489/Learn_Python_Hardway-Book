8568fcf45ec15061d99b28b99ae0e0e6092f9e19
PRABHU SUNDARRAJ <43057160+prabhus489@users.noreply.github.com>
2019-02-16 23:55:40 +0530

    Learn Python Hardway
    Practise

---
+++ Ex-9.py
@@ -0,0 +1,15 @@
+days = "Mon Tue Wed Thu Fri Sta Sun"
+months = "\nJan \nFeb \nMar \nApr \nMay \nJun \nAug"
+
+print "Here are the days: ", days
+print "Here are the months: ", months
+
+print """
+There's something going on here.
+with the three double-quotes.
+We'll be able to type as much as we like.
+Even 4 lines if we want, or 5, 0r 6.
+
+-Thanks
+ Prabhu Sundarraj
+"""
---
+++ cmd_line_arg.py
@@ -0,0 +1,11 @@
+# l = [1,2]
+# print(l[-2])
+from sys import argv
+dog, first, second, third, fourth, fifth = argv
+
+print ("The script is called:", dog)
+print ("Your first variable is:", first)
+print ("Your second variable is:", second)
+print ("Your first variable is:", third)
+print ("Your second variable is:", fourth)
+print ("Your first variable is:", fifth)
