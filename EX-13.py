d8f96ef249c59a7108bf4628cdd79945c7a0028f
PRABHU SUNDARRAJ <43057160+prabhus489@users.noreply.github.com>
2019-02-16 23:58:33 +0530

    Learn Python Hardway
    Practise

---
+++ EX-13.py
@@ -0,0 +1,12 @@
+from sys import argv
+dog, first, second, third = argv
+
+print ("The script is called:", dog)
+print ("Your first variable is:", first)
+print ("Your second variable is:", second)
+print ("Your third variable is:", third)
+
+
+#run this program in Command line using this line ---> python EX-13.py first 2nd 3rd
+# need to pass argument variable in run command
+#python EX-13.py first 2nd 3rd
